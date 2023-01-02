239964a40e4b3a9ada10b316c4b265b4b05c80b9
Mahmoud Kamal Fawzy <65209396+mahmoud947@users.noreply.github.com>
2023-01-02 21:51:42 +0200

    implement getHero by id (useCase)

---
+++ hero/hero-interactors/src/main/java/com/example/hero_interactors/GetHero.kt
@@ -0,0 +1,37 @@
+package com.example.hero_interactors
+
+import com.example.core.DataState
+import com.example.core.ProgressBarState
+import com.example.core.UiComponent
+import com.example.hero_datasource.cache.HeroCache
+import com.example.hero_domain.Hero
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+
+class GetHero(
+    private val cache: HeroCache
+) {
+    operator fun invoke(heroId: Int): Flow<DataState<Hero>> = flow {
+        try {
+            emit(DataState.Loading<Hero>(progressBarState = ProgressBarState.Loading))
+            val hero: Hero = cache.getHero(id = heroId)
+            if (hero == null) {
+                throw Exception("hero not found")
+            }
+            emit(DataState.Data<Hero>(hero))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            emit(
+                DataState.Response<Hero>(
+                    uiComponent = UiComponent.Dialog(
+                        title = "Error",
+                        description = e.message ?: "Unknown Error"
+                    )
+                )
+            )
+        } finally {
+            emit(DataState.Loading<Hero>(progressBarState = ProgressBarState.Idle))
+        }
+    }
+}
--- hero/hero-interactors/src/main/java/com/example/hero_interactors/HeroInteractors.kt
+++ hero/hero-interactors/src/main/java/com/example/hero_interactors/HeroInteractors.kt
@@ -5,17 +5,21 @@ import com.example.hero_datasource.network.HeroService
 import com.squareup.sqldelight.db.SqlDriver
 
 data class HeroInteractors(
-    val getHeros: GetHeros
-){
-    companion object Factory{
-        fun build(sqlDriver: SqlDriver):HeroInteractors{
+    val getHeros: GetHeros,
+    val getHero: GetHero
+) {
+    companion object Factory {
+        fun build(sqlDriver: SqlDriver): HeroInteractors {
+            val cache = HeroCache.build(sqlDriver)
             return HeroInteractors(
                 getHeros = GetHeros(
                     service = HeroService.build(),
-                    cache = HeroCache.build(sqlDriver)
-                )
+                    cache = cache
+                ),
+                getHero = GetHero(cache = cache)
             )
         }
+
         val schema: SqlDriver.Schema = HeroCache.schema
 
         val dbName: String = HeroCache.dbName
