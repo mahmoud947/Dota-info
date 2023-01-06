ae79f847afb555bb2e4a61fdf749d95dba288745
Mahmoud Kamal Fawzy <65209396+mahmoud947@users.noreply.github.com>
2023-01-06 15:50:06 +0200

    add HeroListToolbar

--- hero/ui-heroList/src/main/java/com/example/ui_herolist/ui/HeroList.kt
+++ hero/ui-heroList/src/main/java/com/example/ui_herolist/ui/HeroList.kt
@@ -3,40 +3,53 @@ package com.example.ui_herolist
 import HeroListItem
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
 import coil.ImageLoader
 import com.example.core.ProgressBarState
 import com.example.hero_domain.Hero
+import com.example.ui_herolist.components.HeroListToolbar
 import com.example.ui_herolist.ui.HeroListState
 
+@OptIn(ExperimentalComposeUiApi::class)
 @ExperimentalAnimationApi
 @Composable
 fun HeroList(
     state: HeroListState,
     imageLoader: ImageLoader,
-    navigateToDetailScreen:(Int)->Unit,
+    navigateToDetailScreen: (Int) -> Unit,
 ) {
 
 
     Box(modifier = Modifier.fillMaxSize()) {
-        LazyColumn {
-            items(state.heros) { hero: Hero ->
-                HeroListItem(
-                    hero = hero,
-                    imageLoader = imageLoader,
-                    onSelectHero = {heroId->
-                        navigateToDetailScreen(heroId)
-                    }
-                )
+        Column {
+            val heroName: MutableState<String> = remember {
+                mutableStateOf("")
             }
+
+            HeroListToolbar(heroName = heroName.value, onHeroNameChanged = {
+                heroName.value = it
+            }, onExecuteSearch = { /*TODO*/ }, onShowFilterDialog = {})
+            LazyColumn {
+                items(state.heros) { hero: Hero ->
+                    HeroListItem(hero = hero, imageLoader = imageLoader, onSelectHero = { heroId ->
+                        navigateToDetailScreen(heroId)
+                    })
+                }
+            }
+
         }
+
         if (state.progressBarState is ProgressBarState.Loading) {
             CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
         }
